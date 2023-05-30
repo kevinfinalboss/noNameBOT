e12c746e3e0436f28ae133e4cc2e8eee4c30d859
Kevin Gomes <kevinmg50@gmail.com>
2023-05-30 17:43:55 -0300

    fix: corrigido invalid api key

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { Client, Collection, Partials, GatewayIntentBits } = require('discord.js');
 const config = require('./Config/config.json');
 const handler = require("./handler/index");
-const handlerError = require("./handler/AntiCrash");
+const handlerError = require("./handler/antiCrash");
 const updateApplicationStatus = require('./handler/status');
+const updateApplicationStatus2 = require('./handler/status2')
+const updateApplicationStatus3 = require('./handler/status3')
 
 const myIntents = [
   GatewayIntentBits.Guilds,
@@ -46,9 +48,9 @@ handlerError.antiCrash(client);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
-  updateApplicationStatus(client, '1110238720143147048', config.appId, 'IP-Monitoring');
-  updateApplicationStatus(client, '1110284270712401990', config.appId2, 'DudinhaBOT');
-  updateApplicationStatus(client, '1110287873619529929', config.appId3, 'SasaBOT');
+  updateApplicationStatus(client);
+  updateApplicationStatus2(client);
+  updateApplicationStatus3(client);
 });
 
 client.login(config.Bot_Token);
