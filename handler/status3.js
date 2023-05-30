b621b988d0f3e28ecd2b66fb3ff365bc5f0544c6
Kevin Gomes <kevinmg50@gmail.com>
2023-05-30 17:44:15 -0300

    refactor: refatorado embed para monitoramento

--- handler/status3.js
+++ handler/status3.js
@@ -1,25 +1,26 @@
+const { EmbedBuilder } = require('discord.js');
 const axios = require('axios');
 const config = require('../Config/config.json');
-const { EmbedBuilder } = require('@discordjs/builders');
 
+const channelId2 = '1110287873619529929';
 const interval = 10000;
+
 let message;
 
 async function updateApplicationStatus(client) {
   const apiKey = config.apiKey;
-  const appId = config.appId;
-  const channelId = '1110238720143147048';
-  const interval = 10000;
+  const appId2 = config.appId3;
 
-  const channel = await client.channels.fetch(channelId);
+  const channel = await client.channels.fetch(channelId2);
+
   if (!channel) {
-    console.error(`Canal com ID '${channelId}' não encontrado.`);
+    console.error(`Canal com ID '${channelId2}' não encontrado.`);
     return;
   }
 
   const updateMessage = async () => {
     try {
-      const response = await axios.get(`https://api.squarecloud.app/v2/apps/${appId}/status`, {
+      const response = await axios.get(`https://api.squarecloud.app/v2/apps/${appId2}/status`, {
         headers: {
           Authorization: apiKey
         }
@@ -30,9 +31,10 @@ async function updateApplicationStatus(client) {
 
         const uptime = status.uptime ? Math.floor(status.uptime / 1000) : 0;
         const minutes = Math.floor(uptime / 60);
-        
+
         const embed = new EmbedBuilder()
-          .setTitle('Status da aplicação: IP-Monitoring')
+          .setColor('#2f3136')
+          .setTitle('Status da aplicação: SasaBOT')
           .setImage('https://cdn.dribbble.com/users/662779/screenshots/5122311/media/29ad313d90c405edb25c5fac6e40a899.gif')
           .addFields(
             { name: '💻 CPU', value: status.cpu ? status.cpu.toString() : 'N/A' },
@@ -45,24 +47,16 @@ async function updateApplicationStatus(client) {
           )
           .setTimestamp()
           .setFooter({
-            text: "Desenvolvido por: kevinfinalboss",
-            iconURL:
-                "https://avatars.githubusercontent.com/u/88814728?s=400&u=0bb6a0790758c0cc121c8aeafe2cd1237fa151f8&v=4",
-        })
+            text: 'Desenvolvido por: kevinfinalboss',
+            iconURL: 'https://avatars.githubusercontent.com/u/88814728?s=400&u=0bb6a0790758c0cc121c8aeafe2cd1237fa151f8&v=4'
+          });
+
         if (!message) {
           const messages = await channel.messages.fetch();
           await channel.bulkDelete(messages);
           message = await channel.send({ embeds: [embed] });
         } else {
-          try {
-            await message.edit({ embeds: [embed] });
-          } catch (error) {
-            if (error.code === 10008) {
-              message = await channel.send({ embeds: [embed] });
-            } else {
-              console.error("Erro ao editar a mensagem:", error);
-            }
-          }
+          await message.edit({ embeds: [embed] });
         }
       } else {
         console.error('Erro ao obter o status da aplicação:', response.data);
